744f6ece9451bd6b9f1d3877b0510052fa5e8336
LuckyFalcon <davidfalcon518@gmail.com>
2020-08-07 01:09:09 +0200

    Added SaveTrip
    Added saving a trip

---
+++ saveTrip/helpers/Auth.js
@@ -0,0 +1,26 @@
+const { firebase, admin } = require('../FireBaseConfig');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer', '').trim()
+        if (token) {
+            admin.auth().verifyIdToken(token)
+                .then(function (decodedToken) {
+                    req.user = decodedToken.uid
+                    return next()
+                })
+                .catch(function (error) {
+                    //Couldn't decode the token
+                    return res.status(500).json({ error: 'Something went wrong' })
+                });
+        } else {
+
+            //Couldn't find the token
+            return res.status(500).json({ error: 'Something went wrong' })
+        }
+    } catch (error) {
+
+        //Something went wrong during getting the token and decoding
+        return res.status(500).json({ error: 'Something went wrong' })
+    }
+};
---
+++ saveTrip/saveTrip/index.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+const Auth = require('../helpers/Auth');
+const { check, validationResult } = require('express-validator');
+const SQLQueries = require('../helpers/InsertReport.js');
+const createHandler = require("azure-function-express").createHandler;
+const express = require("express");
+
+// Create express app as usual
+const app = express();
+
+app.use(Auth);
+
+app.get("/saveTrip", [
+  check('GID')
+    .isString()
+    .not().isEmpty(),
+
+], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  try {
+      
+    var GID = req.query.GID;
+
+    await SQLQueries.UpdateTrip(req.user, GID);
+
+    return res.status(200).json({ success: 'success' })
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: 'Error' })
+  }
+});
+
+
+
+
+module.exports = createHandler(app)
+
+
+
